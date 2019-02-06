4e804c2f7ebee4640e15fba51ee62e278461ea3c
leonardo Kaplan <kaplanleonardo@gmail.com>
2019-02-06 14:51:25 -0600

    Finish toy app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
   root 'application#hello'
+end
+
+Rails.application.routes.draw do
+  resources :microposts
+  resources :users
+  root 'users#index'
 end
